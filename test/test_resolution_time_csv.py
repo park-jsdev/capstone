def57d252006e28ab33310845f297ec9d36c28fb
Terry Park <58895710+park-jsdev@users.noreply.github.com>
2023-03-31 10:22:22 -0700

    update resolution time test

--- test/test_resolution_time_csv.py
+++ test/test_resolution_time_csv.py
@@ -6,11 +6,15 @@ import pytest
 #        python -m pytest test_resolution_time_csv.py --test_data_file=<path_to_test_data_file> --output_data_file=<path_to_output_data_file>
 #        Replace <path_to_test_data_file> and <path_to_output_data_file> with the paths to the test data and output data csv file.
 
-# Constants for column names
+import pandas as pd
+import pytest
+
+# Define the column names as variables
 START_DATE_COL = 'Start Date'
 END_DATE_COL = 'End Date'
-EXPECTED_AVG_COL = 'Expected Avg'
+EXPECTED_AVG_RES_TIME_COL = 'Expected Avg Resolution Time'
 MONTHS_COL = 'Months'
+EXPECTED_AVG_COL = 'Expected Avg'
 
 # Fixture to load the test data csv file
 @pytest.fixture
@@ -24,24 +28,24 @@ def output_data(output_data_file):
 
 # test the output's average resolution time vs the actual resolution time from the test data
 def test_average_resolution_time_total(test_data, output_data):
-    days_diff = (pd.to_datetime(test_data['End Date']) - pd.to_datetime(test_data['Start Date'])).dt.days
+    days_diff = (pd.to_datetime(test_data[END_DATE_COL]) - pd.to_datetime(test_data[START_DATE_COL])).dt.days
     avg_days_diff = days_diff.mean()
-    expected_avg_days_diff = output_data['Expected Avg Resolution Time'].iloc[0]
+    expected_avg_days_diff = output_data[EXPECTED_AVG_RES_TIME_COL].iloc[0]
     assert avg_days_diff == pytest.approx(expected_avg_days_diff, rel=1e-2)
 
 # test the output's average resolution time per month vs the actual resolution time per month from the test data
 def test_average_resolution_time_per_month(test_data, output_data):
     # Convert date columns to datetime type
-    test_data['Start Date'] = pd.to_datetime(test_data['Start Date'])
-    test_data['End Date'] = pd.to_datetime(test_data['End Date'])
+    test_data[START_DATE_COL] = pd.to_datetime(test_data[START_DATE_COL])
+    test_data[END_DATE_COL] = pd.to_datetime(test_data[END_DATE_COL])
     
     # Calculate days difference and group by month
-    test_data['Days Difference'] = (test_data['End Date'] - test_data['Start Date']).dt.days
-    grouped = test_data.groupby(test_data['Start Date'].dt.month)['Days Difference']
+    test_data['Days Difference'] = (test_data[END_DATE_COL] - test_data[START_DATE_COL]).dt.days
+    grouped = test_data.groupby(test_data[START_DATE_COL].dt.month)['Days Difference']
 
     # Calculate actual average for each month
     actual_avg_days_diff_per_month = grouped.mean()
 
     # Check if the calculated values match the expected values
-    expected_avg_days_diff_per_month = output_data.set_index('Months')['Expected Avg'].to_dict()
+    expected_avg_days_diff_per_month = output_data.set_index(MONTHS_COL)[EXPECTED_AVG_COL].to_dict()
     assert actual_avg_days_diff_per_month.to_dict() == pytest.approx(expected_avg_days_diff_per_month, rel=1e-2)
